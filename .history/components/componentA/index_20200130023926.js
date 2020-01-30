26fa71fce642ec522f08c657b6f9c905f2aa1658
Omar Farouk Lakhdhar <omarlakhdhar@gmail.com>
2020-01-30 02:40:42 +0100

    initial commit

--- .history/components/componentA/index_20200130023926.js
+++ .history/components/componentA/index_20200130023926.js
@@ -10,7 +10,7 @@ class Emitter extends Component {
 
   componentDidMount() {}
 
-  _emitEvent = () => {
+  _emitEvent() {
     console.log("TRYING TO EMIT FROM COMPONENT A");
     this.eventEmitter.emit("PLAYAUDIO");
   }
